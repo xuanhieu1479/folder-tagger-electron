e833281632c7c2d348ce92b38e61255af72a76b3
Pham Xuan Hieu <xuanhieu1479@gmail.com>
2020-12-21 13:06:29 +0700

    Integrate react.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from 'electron';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
