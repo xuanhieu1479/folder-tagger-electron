6be052a79b3a2c546cc4c10c2bd9f80af8063a28
Xuan Hieu <xuanhieu1479@gmail.com>
2021-02-10 02:10:03 +0700

    Prevent tags with special characters from being created.

--- src/common/variables/message.ts
+++ src/common/variables/message.ts
@@ -4,7 +4,8 @@ const MESSAGE = {
   SPECIFIC_FOLDER_ALREADY_EXISTS: (folderlocation: string): string =>
     `${folderlocation}\nalready exists in database`,
   INVALID_PARAMS: 'The parameters are invalid',
-  CANNOT_EDIT_MANY_FOLDERS: 'Only one folder can be edited at a time!'
+  CANNOT_EDIT_MANY_FOLDERS: 'Only one folder can be edited at a time!',
+  SPECIAL_CHARACTERS_FORBIDDEN: 'Special characters are not allowed!'
 };
 
 export default MESSAGE;
--- src/fe/components/CustomMultiSelect.tsx
+++ src/fe/components/CustomMultiSelect.tsx
@@ -1,6 +1,8 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React, { ReactElement, ReactNode, useState } from 'react';
 import { MenuItem } from '@blueprintjs/core';
 import { MultiSelect, ItemRenderer } from '@blueprintjs/select';
+import { MESSAGE } from '../../common/variables/commonVariables';
+import { showMessage } from '../../utilities/showMessage';
 import './styles/CustomMultiSelect.styled.scss';
 
 interface CustomMultiSelectInterface {
@@ -12,6 +14,7 @@ interface CustomMultiSelectInterface {
     newSelectedItems: Array<string>
   ) => void;
 }
+const noSpecialCharactersRegex = new RegExp(/[^A-Za-z0-9\s]/g);
 
 const CustomMultiSelect = ({
   itemKey,
@@ -19,6 +22,16 @@ const CustomMultiSelect = ({
   selectedItems,
   updateSelectedItems
 }: CustomMultiSelectInterface): ReactElement => {
+  const [inputValue, setInputvalue] = useState('');
+
+  const clearInput = () => {
+    setInputvalue('');
+  };
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    if (!noSpecialCharactersRegex.test(value)) setInputvalue(value);
+    else showMessage.error(MESSAGE.SPECIAL_CHARACTERS_FORBIDDEN);
+  };
   const onItemSelect = (item: string): void => {
     if (selectedItems.includes(item))
       updateSelectedItems(
@@ -26,15 +39,17 @@ const CustomMultiSelect = ({
         selectedItems.filter(t => t !== item)
       );
     else updateSelectedItems(itemKey, [...selectedItems, item]);
+    clearInput();
   };
   const onRemoveItem = (item: ReactNode): void => {
     updateSelectedItems(
       itemKey,
       selectedItems.filter(t => t !== item)
     );
+    clearInput();
   };
   const onCreatItem = (query: string) => {
-    return query;
+    return query.replace(noSpecialCharactersRegex, '');
   };
 
   const renderSelectItems: ItemRenderer<string> = (
@@ -55,27 +70,33 @@ const CustomMultiSelect = ({
     query: string,
     active: boolean,
     handleClick: React.MouseEventHandler<HTMLElement>
-  ) => (
-    <MenuItem
-      icon="add"
-      text={`Create "${query}" tag`}
-      active={active}
-      onClick={handleClick}
-    />
-  );
-
+  ) => {
+    const sanitizedQuery = query.replace(noSpecialCharactersRegex, '');
+    return (
+      <MenuItem
+        icon="add"
+        text={`Create "${sanitizedQuery}" tag`}
+        active={active}
+        onClick={handleClick}
+      />
+    );
+  };
   return (
     <MultiSelect
       fill={true}
       resetOnSelect={true}
       items={allItems}
+      query={inputValue}
       selectedItems={selectedItems}
       itemRenderer={renderSelectItems}
       onItemSelect={onItemSelect}
       createNewItemRenderer={renderCreateItems}
       createNewItemFromQuery={onCreatItem}
       tagRenderer={item => item}
-      tagInputProps={{ onRemove: onRemoveItem }}
+      tagInputProps={{
+        onRemove: onRemoveItem,
+        inputProps: { value: inputValue, onChange: onInputChange }
+      }}
       popoverProps={{
         minimal: true,
         popoverClassName: 'custom-multi-select_popover'
