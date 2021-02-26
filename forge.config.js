e32ecc49ed3f2a2d0857cfd9f37caf23b60ac1d3
xuanhieu1479 <xuanhieu1479@gmail.com>
2021-02-26 16:49:04 +0700

    Move electron forge config to external file.

---
+++ forge.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  packagerConfig: {
+    executableName: 'Folder Tagger',
+    name: 'Folder Tagger'
+  },
+  makers: [{ name: '@electron-forge/custom-maker' }],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      platforms: ['win32'],
+      config: {
+        repository: {
+          owner: 'xuanhieu1479',
+          name: 'folder-tagger-electron'
+        }
+      }
+    }
+  ],
+  plugins: [
+    [
+      '@electron-forge/plugin-webpack',
+      {
+        mainConfig: './webpack.main.config.js',
+        renderer: {
+          config: './webpack.renderer.config.js',
+          entryPoints: [
+            {
+              html: './src/index.html',
+              js: './src/renderer.ts',
+              name: 'main_window'
+            }
+          ]
+        }
+      }
+    ]
+  ]
+};
